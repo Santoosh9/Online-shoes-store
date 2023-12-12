a7b7234a4558c85b95af9344971b9d173db6bb65
Santoosh9 <santoshgajurel17@gmail.com>
2023-12-12 23:15:34 +0545

    login and register form

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from '../layouts/authentication/Register';
+import Login from '../layouts/authentication/Login';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
